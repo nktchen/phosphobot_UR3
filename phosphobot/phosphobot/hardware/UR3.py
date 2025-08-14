054759ce987294a483b4a8d8ae8d717f73c8a495
nktchen <n.chen@edu.centraluniversity.ru>
2025-08-14 13:27:58 +0300

    add file for UR3 with code from tutorial

---
+++ phosphobot/phosphobot/hardware/UR3.py
@@ -0,0 +1,8 @@
+from phosphobot.hardware.base import BaseRobot
+
+class UR3(BaseRobot):
+    def __init__(self, config):
+        super().__init__(config)
+        # Your initialization here
+
+    ... # Implement the BaseRobot's abstract methods here
