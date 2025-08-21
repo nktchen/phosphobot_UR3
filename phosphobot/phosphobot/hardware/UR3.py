c7c1fe5a773cb0f7b8dab6ef88268e2ffff291fc
nktchen <chennikita70@gmail.com>
2025-08-21 12:06:54 +0300

    mini commit

--- phosphobot/phosphobot/hardware/UR3.py
+++ phosphobot/phosphobot/hardware/UR3.py
@@ -1,8 +1,8 @@
 from phosphobot.hardware.base import BaseRobot
 
 class UR3(BaseRobot):
+    name = "UR3"
     def __init__(self, config):
         super().__init__(config)
-        # Your initialization here
 
-    ... # Implement the BaseRobot's abstract methods here
+    
